c0bd8ff900157d7b7ace5b64921135fd13d50e26
Tony Mamo <tmamo13991@gmail.com>
2022-08-31 11:18:10 -0400

    add segment and configure api key

--- libs/utils/src/track-events/track-events.ts
+++ libs/utils/src/track-events/track-events.ts
@@ -1,16 +1,17 @@
-/**
- * Need to make sure window and window.analytics are available before firing off track(),
- * so this utility saves us a few lines of code each time
- *
- * @param event
- * @param properties
- */
-export default function trackEvent(
-  event: string, properties?: Record<string, any>
-) {
-  if (window && window.analytics) {
-    return window.analytics.track(event, properties);
-  }
+import { AnalyticsBrowser } from '@segment/analytics-next'
 
-  return null;
+const analytics = AnalyticsBrowser.load({ writeKey: process.env.NX_PROD_SEGMENT_API_KEY || '' })
+
+/**
+ * Send an event to Segment for analytics tracking
+ * @param event Name of event
+ * @param properties Any additional properties to pass along with the track call
+ */
+const trackEvent = (event: string, properties?: Record<string, unknown>) => {
+  if (process.env.NODE_ENV === 'production') {
+    return analytics.track(event, properties)
+  }
+  return null
 }
+
+export default trackEvent
