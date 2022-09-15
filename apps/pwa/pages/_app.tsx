025af63495546ea3c4f6dd5e66e7d4dc7f6e9f91
Juozas Beniusis <beniusij@gmail.com>
2022-09-15 22:34:30 +0100

    Remove default styles

--- apps/pwa/pages/_app.tsx
+++ apps/pwa/pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import './styles.css'
 import { Layout } from '@getpackup-group/components'
 import { ReduxWrapper } from '@getpackup-group/redux'
 import { HelmetProvider } from 'react-helmet-async'
