45a8c7b90b0449325d42ef45aee4d8a227b8fbbb
Juozas Beniusis <beniusij@gmail.com>
2022-09-18 22:04:21 +0100

    Redux - update env vars to start with NX_

--- libs/redux/src/ReduxWrapper.tsx
+++ libs/redux/src/ReduxWrapper.tsx
@@ -16,75 +16,78 @@ import { initialState as clientInitialState } from './ducks/client'
 import configureStore from './configureStore'
 import { initialState as globalAlertsInitialState } from './ducks/globalAlerts'
 
-export const initialState = process.env.BROWSER // eslint-disable-next-line no-underscore-dangle
-  ? window.__INITIAL_STATE__
-  : {
+export const onWorkerUpdateReady = () => store.dispatch(showWorkerUpdateModal())
+
+export function ReduxWrapper(props) {
+  console.log(process.env)
+
+  const initialState = process.env.BROWSER // eslint-disable-next-line no-underscore-dangle
+    ? window.__INITIAL_STATE__
+    : {
       firestore: {} as typeof FirestoreReducer,
       firebase: {} as FirebaseReducer.Reducer<any, any>,
       client: clientInitialState,
       globalAlerts: globalAlertsInitialState,
       workerUpdateReady: workerUpdateInitialState,
     }
-const { store, persistor } = configureStore(initialState)
+  const { store, persistor } = configureStore(initialState)
 
-const rrfConfig = {
-  userProfile: 'users',
-  useFirestoreForProfile: true,
-}
+  const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true,
+  }
 
-const rrfProps = {
-  firebase,
-  config: rrfConfig,
-  dispatch: store.dispatch,
-  createFirestoreInstance, // since we are using Firestore
-}
+  const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance, // since we are using Firestore
+  }
 
-const firebaseConfig = {
-  apiKey:
-    process.env.SITE_URL === 'https://getpackup.com'
-      ? process.env.FIREBASE_API_KEY
-      : process.env.FIREBASE_TEST_API_KEY,
-  authDomain:
-    process.env.SITE_URL === 'https://getpackup.com'
-      ? process.env.FIREBASE_AUTH_DOMAIN
-      : process.env.FIREBASE_TEST_AUTH_DOMAIN,
-  databaseURL:
-    process.env.SITE_URL === 'https://getpackup.com'
-      ? process.env.FIREBASE_DATABASE_URL
-      : process.env.FIREBASE_TEST_DATABASE_URL,
-  projectId:
-    process.env.SITE_URL === 'https://getpackup.com'
-      ? process.env.FIREBASE_PROJECT_ID
-      : process.env.FIREBASE_TEST_PROJECT_ID,
-  storageBucket:
-    process.env.SITE_URL === 'https://getpackup.com'
-      ? process.env.FIREBASE_STORAGE_BUCKET
-      : process.env.FIREBASE_TEST_STORAGE_BUCKET,
-  messagingSenderId:
-    process.env.SITE_URL === 'https://getpackup.com'
-      ? process.env.FIREBASE_MESSAGING_SENDER_ID
-      : process.env.FIREBASE_TEST_MESSAGING_SENDER_ID,
-  appId:
-    process.env.SITE_URL === 'https://getpackup.com'
-      ? process.env.FIREBASE_APP_ID
-      : process.env.FIREBASE_TEST_APP_ID,
-}
+  const firebaseConfig = {
+    apiKey:
+      process.env.NX_SITE_URL === 'https://getpackup.com'
+        ? process.env.NX_FIREBASE_API_KEY
+        : process.env.NX_FIREBASE_TEST_API_KEY,
+    authDomain:
+      process.env.NX_SITE_URL === 'https://getpackup.com'
+        ? process.env.NX_FIREBASE_AUTH_DOMAIN
+        : process.env.NX_FIREBASE_TEST_AUTH_DOMAIN,
+    databaseURL:
+      process.env.NX_SITE_URL === 'https://getpackup.com'
+        ? process.env.NX_FIREBASE_DATABASE_URL
+        : process.env.NX_FIREBASE_TEST_DATABASE_URL,
+    projectId:
+      process.env.NX_SITE_URL === 'https://getpackup.com'
+        ? process.env.NX_FIREBASE_PROJECT_ID
+        : process.env.NX_FIREBASE_TEST_PROJECT_ID,
+    storageBucket:
+      process.env.NX_SITE_URL === 'https://getpackup.com'
+        ? process.env.NX_FIREBASE_STORAGE_BUCKET
+        : process.env.NX_FIREBASE_TEST_STORAGE_BUCKET,
+    messagingSenderId:
+      process.env.NX_SITE_URL === 'https://getpackup.com'
+        ? process.env.NX_FIREBASE_MESSAGING_SENDER_ID
+        : process.env.NX_FIREBASE_TEST_MESSAGING_SENDER_ID,
+    appId:
+      process.env.NX_SITE_URL === 'https://getpackup.com'
+        ? process.env.NX_FIREBASE_APP_ID
+        : process.env.NX_FIREBASE_TEST_APP_ID,
+  }
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig)
-}
+  if (!firebase.apps.length) {
+    console.log(firebaseConfig)
+    firebase.initializeApp(firebaseConfig)
+  }
 
-if (process.env.ENVIRONMENT === 'DEVELOP') {
-  // eslint-disable-next-line no-console
-  // console.log(`Development Env: Using Firestore Emulator`);
-  // firebase.firestore().useEmulator('localhost', 8083);
-} else {
-  firebase.firestore()
-}
+  if (process.env.ENVIRONMENT === 'DEVELOP') {
+    // eslint-disable-next-line no-console
+    // console.log(`Development Env: Using Firestore Emulator`);
+    // firebase.firestore().useEmulator('localhost', 8083);
+  } else {
+    firebase.firestore()
+  }
 
-export const onWorkerUpdateReady = () => store.dispatch(showWorkerUpdateModal())
-
-export function ReduxWrapper(props) {
   return (
     <Provider store={store}>
       {typeof window !== 'undefined' ? (
