36be1aa350529d2f9251d155d5257f1e8c1b28e5
Tony Mamo <tmamo13991@gmail.com>
2023-01-16 20:19:08 -0500

    404 page

---
+++ apps/pwa/pages/404.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Box, Button, Column, Heading, PageContainer, Row } from '@getpackup-group/components'
+import { trackEvent } from '@getpackup-group/utils'
+import Head from 'next/head'
+
+export default function NotFound() {
+  return (
+    <PageContainer withVerticalPadding>
+      <Head>
+        <title>Page Not Found | Packup</title>
+      </Head>
+      <Row>
+        <Column md={8} mdOffset={2}>
+          <Box>
+            <Heading>Sorry, something went wrong.</Heading>
+            <p>
+              We could not find the page you were looking for. Please try again or visit the home
+              page.
+            </p>
+            <Button
+              type="link"
+              to="/"
+              color="primary"
+              onClick={() => trackEvent('404 Home Button Clicked')}
+            >
+              Home
+            </Button>
+          </Box>
+        </Column>
+      </Row>
+    </PageContainer>
+  )
+}
