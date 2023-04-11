a07fc96a44432997526d82b5142b4722f751bedf
Tony Mamo <tmamo13991@gmail.com>
2023-04-11 11:31:59 -0400

    add user.set() on new trip page to fix missing user on trip gen page

--- apps/pwa/pages/trips/new.tsx
+++ apps/pwa/pages/trips/new.tsx
@@ -1,8 +1,37 @@
 import { Box, Column, Heading, PageContainer, Row, TripFormCreate } from '@packup/components'
+import { AppState } from '@packup/redux'
 import { fontSizeSmall } from '@packup/styles'
 import Head from 'next/head'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useFirebase } from 'react-redux-firebase'
 
 export default function NewTripSummary() {
+  const auth = useSelector((state: AppState) => state.firebase.auth)
+  const firebase = useFirebase()
+
+  const user = firebase.auth().currentUser
+  useEffect(() => {
+    // handle missing user info from signup creation not finishing
+    if (!user || !user.uid)
+      firebase
+        .firestore()
+        .collection('users')
+        .doc(auth.uid)
+        .set({
+          uid: user.uid,
+          email: auth.email,
+          displayName: auth.providerData[0].displayName,
+          username: auth.providerData[0].displayName.replace(/[^A-Z0-9]/gi, '').toLowerCase(),
+          photoURL: auth.providerData[0].photoURL,
+          bio: '',
+          website: '',
+          location: '',
+          lastUpdated: new Date(),
+          createdAt: new Date(),
+        })
+  }, [user, auth, firebase])
+
   return (
     <PageContainer>
       <Box>
