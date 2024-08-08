ba66cea0da7480c6ff4fa9da1c7783141dd420fc
Tony Mamo <tmamo13991@gmail.com>
2024-08-08 11:53:39 -0400

    move ad in past trips section to 3rd position

--- libs/components/src/trip/ListView.tsx
+++ libs/components/src/trip/ListView.tsx
@@ -85,8 +85,18 @@ export function ListView({ trips, auth }: { trips: TripType[]; auth: FirebaseRed
           <Heading as="h2" altStyle>
             Past Trips
           </Heading>
-          {pastTrips.map((trip) => renderTrip(trip))}
-          {pastTrips.length > 3 && <PackingListBannerAd location="trips" />}
+          {pastTrips.map((trip, index) => {
+            if (index === 2) {
+              return (
+                <>
+                  <PackingListBannerAd location="trips" />
+                  {renderTrip(trip)}
+                </>
+              )
+            } else {
+              return renderTrip(trip)
+            }
+          })}
         </>
       )}
     </>
